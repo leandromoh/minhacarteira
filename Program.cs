6546898d0cc1908a2adc925a27ff243821343168
unknown <leandromoh@hotmail.com>
2020-10-25 11:36:32 -0300

    input and output paths in appsettings

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using static consoleapp.Calc;
 
 namespace consoleapp
 {
     public static class Program
     {
-        static async Task Main(string[] args)
+        public static IConfigurationRoot Configuration { get; }
+
+        static Program()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        static void Main(string[] args)
         {
             var culture = new CultureInfo("pt-BR");
-            var ops = ParserOperacao.ParseCSV(@"C:\Users\xxxxxx\Desktop\xxxxxx.txt", culture);
-            var fileName = @$"C:\Users\xxxxxx\Desktop\carteira.{DateTime.Now:yyyy.MM.dd.HH.mm.ss}.html";
+            var operationsFilePath = Configuration["input:operationsFilePath"];
+            var reportDirectoryPath = Configuration["output:reportDirectoryPath"];
+            var reportFilePath =  Path.Combine(reportDirectoryPath, 
+                                        $"minhacarteira.{DateTime.Now:yyyy.MM.dd.HH.mm.ss}.html");
+
+            var ops = ParserOperacao.ParseCSV(operationsFilePath, culture);
 
             var gruposAtivo = ops.GroupBy(op => op.Ativo.GetTipoAtivo());
             var carteiras = gruposAtivo
@@ -21,7 +38,7 @@ namespace consoleapp
 
             var carteiraRV = CarteiraMaster(carteiras, "RV");
 
-            carteiras.Prepend(carteiraRV).SaveAsHTML(fileName);
+            carteiras.Prepend(carteiraRV).SaveAsHTML(reportFilePath);
         }
     }
 }
